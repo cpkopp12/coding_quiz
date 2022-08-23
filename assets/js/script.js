2c572ad10504c7f1cf839d7150fc5afae867148a
cpkopp <cameronpkopp@gmail.com>
2022-08-23 11:30:58 -0400

    correct/incorrect and timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 // parent.empty() removes child elements from parent
 var mainEls = document.querySelector("#main-els");
 var mainTitle = document.querySelector("#question");
+var timerEl = document.querySelector("#timer");
 var qcount = 0;
+var qsCorrect = 0;
+var qsIncorrect = 0;
+var rightWrong = document.createElement("p");
+var timerCount = 60;
+console.log(timerEl);
 
 var q1 = {
     question : "Question 1",
@@ -19,7 +25,26 @@ var q2 = {
     choiceD : "choice d2",
     answer : "choice_a"
 }
-var qsArray = [q1,q2];
+
+var q3 = {
+    question : "Question 3",
+    choiceA : "choice a3",
+    choiceB : "choice b3",
+    choiceC : "choice c3",
+    choiceD : "choice d3",
+    answer : "choice_b"
+}
+
+var q4 = {
+    question : "Question 4",
+    choiceA : "choice a4",
+    choiceB : "choice b4",
+    choiceC : "choice c4",
+    choiceD : "choice d4",
+    answer : "choice_a"
+}
+
+var qsArray = [q1,q2,q3,q4];
 
 // LOAD NEW QUESTION function
 var loadNewQuestion = function(qnum) {
@@ -51,13 +76,34 @@ var loadNewQuestion = function(qnum) {
    
 };
 
-
+var setTimer = function() {
+    timerEl.innerHTML = "time: " + timerCount.toString();
+    timerCount--;
+};
 
 // START BUTTON HANDLER function
-var choiceHandler = function() {
+var choiceHandler = function(event) {
     var qAndChoices = loadNewQuestion(qsArray[qcount]);
+    var qchoice = event.target.getAttribute("id");
+    //because of the way I set the function up, answers check index [qcount-1]
+    if (qcount !== 0) {
+        if (qchoice == qsArray[qcount-1].answer) {
+            qsCorrect++;
+            console.log("correct ");
+            console.log(qsCorrect);
+            rightWrong.textContent = "Correct!";
+        } else {
+            qsIncorrect++;
+            console.log("incorrect ");
+            console.log(qsIncorrect);
+            rightWrong.textContent = "Incorrect!";
+            timerCount -= 10;
+        }
+    } else {
+        setInterval(setTimer,1000);
+    }
     mainTitle.textContent = qAndChoices[0];
-    mainEls.replaceChildren(qAndChoices[1],qAndChoices[2],qAndChoices[3],qAndChoices[4]);
+    mainEls.replaceChildren(qAndChoices[1],qAndChoices[2],qAndChoices[3],qAndChoices[4],rightWrong);
     
     qcount ++;
 };
